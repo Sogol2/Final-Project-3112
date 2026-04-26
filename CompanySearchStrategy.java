9725977cd959ec2c5950636509558a3d7681dd3c
Kylee Keith <kkeith7@charlotte.edu>
2026-04-26 14:37:55 -0400

    Create CompanySearchStrategy.java

---
+++ CompanySearchStrategy.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompanySearchStrategy implements ISearchStrategy {
+    @Override
+    public List<IApplication> search(List<IApplication> apps, String query) {
+        List<IApplication> results = new ArrayList<>();
+        String searchTerm = query.toLowerCase();
+
+        for (IApplication app : apps) {
+            if (app.getPosition().toLowerCase().contains(searchTerm)
+                    || app.getJobType().toLowerCase().contains(searchTerm)) {
+                results.add(app);
+            }
+        }
+
+        return results;
+    }
+}
