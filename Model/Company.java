29f5b260c3c5e30d28212299f21dd05599a102be
Kylee Keith <kkeith7@charlotte.edu>
2026-05-05 16:28:28 -0400

    Update Company.java
    - remove status update and notification responsibilities

--- Model/Company.java
+++ Model/Company.java
@@ -1,4 +1,5 @@
 package Model;
+
 public class Company extends User {
     private String companyName;
 
@@ -7,10 +8,7 @@ public class Company extends User {
         this.companyName = companyName;
     }
 
-    public void postStatus(Application app, ApplicationStatus status, Student student) {
-        app.updateStatus(status);
-
-        //notify the student about the status update
-        student.recieveUpdate("Your application for " + companyName + " has been updated to: " + status);
+    public String getCompanyName() {
+        return companyName;
     }
 }
